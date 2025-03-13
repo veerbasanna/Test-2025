2f39e5fa0b786e56d2d300bf0f4b95d49ca71002
VeereshBasanna <veer.basanna@gmail.com>
2025-03-13 12:05:44 +0530

    Create script.sh

---
+++ jenkins/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#this works only on amaszon-linux or redhat
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install java-17 -y 
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
